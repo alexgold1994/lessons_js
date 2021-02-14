50e66e3bb0e416662028d95fe592e125f2ce444f
Alex <alexgold1994@gmail.com>
2021-02-14 17:56:31 +0200

    add show modal by scroll

--- js/script.js
+++ js/script.js
@@ -137,4 +137,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modalTimerId = setTimeout(openModal, 6000);
 
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
 });
