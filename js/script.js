a5fee4efffe07ce0adbee05d0b4c5cdcf325a3f7
Alex <alexgold1994@gmail.com>
2021-02-15 23:01:00 +0200

    add local database

--- js/script.js
+++ js/script.js
@@ -281,4 +281,10 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+
+    fetch('  http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
